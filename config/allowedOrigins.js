ec81731c6714ea548af4696988ec00d87ecedb04
fraidyEfrat <36214264632@mby.co.il>
2023-02-27 15:24:42 +0200

    change

--- config/allowedOrigins.js
+++
@@ -1,6 +0,0 @@
-//כדיי לאפשר לגשת אליי גם מדומיינם אחרים אך כן ארצה מאובטח לא שכל אחד יוכל לגשת אליי לכן אישרתי לנווד לגשת מלוקל הוסט 3000
-
-const allowedOrigins = [
-    'http://localhost:3000',
-    ]
-    module.exports = allowedOrigins
