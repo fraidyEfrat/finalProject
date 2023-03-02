8036d07feb754d7a50d94caabbcd2d38c1e33520
אפרת יפהן <214264632@mbydomain>
2023-03-02 14:30:00 +0200

    Notes about the commit

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,5 +1,5 @@
 //כדיי לאפשר לגשת אליי גם מדומיינם אחרים אך כן ארצה מאובטח לא שכל אחד יוכל לגשת אליי לכן אישרתי לנווד לגשת מלוקל הוסט 3000
-
+//🎶🎶
 const allowedOrigins = [
     'http://localhost:3000',
     ]
