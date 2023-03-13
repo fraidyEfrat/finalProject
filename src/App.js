2102218e5ca4c080b6c4913c1b0f2edee461e2af
אפרת יפהן <214264632@mbydomain>
2023-03-13 14:14:19 +0200

    Beginning of architecture construction

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import {BrowserRouter as Router, Routes, Route, NavLink} from "react-router-dom"
+import Login from './pages/login';
+import Register from './pages/register';
+import Home from './pages/home';
+import Request from './pages/user/request';
+import Articles from './pages/articles/Articles';
+
+import "./App.css"
+
+
+function App() {
+  
+  return (
+    <div className="App">
+      <Router>
+          <nav className='main-nav'>
+          <NavLink to="/">דף הבית</NavLink>
+          <NavLink to="/register">הרשמה</NavLink>
+          <NavLink to="/login">כניסה</NavLink>
+          <NavLink to="/request">איזור אישי</NavLink>
+          <NavLink to="/articles">מאמרים</NavLink>
+          </nav>
+          <Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/login" element={<Login/>}></Route>
+          <Route path="/register" element={<Register/>}></Route>
+          <Route path="/request" element={<Request/>}></Route> 
+          <Route path="/articles" element={<Articles/>}></Route> 
+          </Routes>
+          
+
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/user/request/index.js
@@ -0,0 +1,75 @@
+import { useState } from "react";
+import { Link,useNavigate } from "react-router-dom";
+import axios from "axios";
+
+
+//
+const UserRequest = ({name,age}) => {
+  const navigate = useNavigate()
+  const [subject,setSubject]=useState("");
+  const [request,setRequest]=useState("");
+ 
+  const [err, setErr] = useState(null);
+  const response="";
+  const date=new Date();
+  const status="send";
+
+  //const id = JSON.parse(sessionStorage.getItem('user')).userId;
+  
+
+   const handleClick=async ()=>{
+    //e.preventDefault();
+      try {  
+          let config = {
+              headers: {
+                'Authorization': 'Bearer ' + localStorage.getItem("token")
+              }
+          } 
+        const data=new Date;
+        const status="request:sent";
+        console.log(data);
+        await axios.post("http://localhost:3600/api/user_request",  {subject,request,data}, config);
+        navigate("/")
+      } catch (err) {
+        console.log(err);
+        setErr(err.response.data?.message);
+      }
+    };
+  //       console.log("in handleClick");
+  //       try {  
+          
+          
+
+
+          
+  //            const res = await axios.post("http://localhost:3600/api/user_request",  {request}, {
+  //              withCredentials: true,
+  //            });
+  //            console.log(res.data)
+
+  //            localStorage.setItem("token", JSON.stringify(res.data.accessToken));
+  //            navigate("/");      
+  //             } catch (err) {
+  //            setErr(err.response.data?.message);
+  //          }
+  // }
+  
+return (
+  
+   <div className="user-request-page">
+    <h1>אזור אישי</h1>
+      
+      <input onChange={(e)=>{setSubject(e.target.value)}} type="text" placeholder=" נא לכתוב את נושא השאלה כאן " ></input><br/><br/>
+      <input onChange={(e)=>{setRequest(e.target.value)}} type="text" placeholder=" נא לכתוב את שאלה כאן " ></input><br/><br/>
+      <button onClick={handleClick}>שליחת השאלה</button> <br/><br/>
+      <label> בקשתך נשלחה בהצלחה ניידעך במייל כאשר בקשתך תטופל בהקדם בהצלחה יום נעים מתנדבות מרכז מידע לבריאות האישה בני-ברק</label>
+      
+      </div>
+    /*
+     
+     */
+   
+)
+}
+
+export default UserRequest
